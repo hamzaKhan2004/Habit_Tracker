44558d7a6db10e4b09088afe01fb20a1115c15eb
hamzaKhan2004 <khanhamzatz@gmail.com>
2025-01-18 15:40:24 +0530

    commented the error code

--- app/lib/connectToDB.ts
+++ app/lib/connectToDB.ts
@@ -1,12 +1,12 @@
-import mongoose from "mongoose";
+// import mongoose from "mongoose";
 
-async function connectToDB() {
-  try {
-    await mongoose.connect(process.env.MONGO_URL as string);
-    console.log("db connected..");
-  } catch (error) {
-    console.log(error);
-  }
-}
+// async function connectToDB() {
+//   try {
+//     await mongoose.connect(process.env.MONGO_URL as string);
+//     console.log("db connected..");
+//   } catch (error) {
+//     console.log(error);
+//   }
+// }
 
-export default connectToDB;
+// export default connectToDB;
