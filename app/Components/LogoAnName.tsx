2e0b4884855a1b3980c38b782c631bbb0d034084
hamzaKhan2004 <khanhamzatz@gmail.com>
2025-01-22 17:53:18 +0530

    LogoAnName Error Solved

--- app/Components/LogoAnName.tsx
+++ app/Components/LogoAnName.tsx
@@ -4,22 +4,22 @@ import { defaultColor } from '@/colors';
 
 function LogoAnName() {
     
-  const backgroundColorObject = {backgroundColor:defaultColor}
+  const backgroundColorObject = {backgroundColor:defaultColor.default}
   return (
     <div className="flex gap-2 items-center sm:justify-start justify-center">
-                            <span className='text-2xl font-light flex items-center gap-2'>
-                                    {/* the icon  */}
-                                <div style={backgroundColorObject} className="p-2 rounded-md">
-                                    <AppIcon color='#ffffff' height='34' width='34'/>
-                                </div>
-                                <span style={{color:"#d90429"}} className='font-bold text-mainColor'>
-                                    Habit
-                                </span>
-                                <span  className='font-light'>
-                                    tracker
-                                </span>
-                            </span>
-                        </div>
+        <span className='text-2xl font-light flex items-center gap-2'>
+                {/* the icon  */}
+            <div style={backgroundColorObject} className="p-2 rounded-md">
+                <AppIcon color='#ffffff' height='34' width='34'/>
+            </div>
+            <span style={{color:"#d90429"}} className='font-bold text-mainColor'>
+                Habit
+            </span>
+            <span  className='font-light '>
+                tracker
+            </span>
+        </span>
+    </div>
   )
 }
 
